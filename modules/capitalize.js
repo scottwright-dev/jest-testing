4ca2ec0efdfa07aef63eb5afc4585f34bdf889f2
Scott Wright <scott.wright.uk@gmail.com>
2023-11-29 09:22:27 +0000

    refactor: capitalize function to pass new tests

--- modules/capitalize.js
+++ modules/capitalize.js
@@ -1,5 +1,15 @@
 function capitalize(string) {
 
+   const firstChar = string.charAt(0).toUpperCase() 
+   const rest = string.slice(1).toLowerCase();
+
+  return firstChar.concat(rest);
+
   }
 
   export default capitalize;
+
+  console.log(capitalize('hello'));
+  console.log(capitalize('fish'));
+  console.log(capitalize('Fish and chips'));
+  console.log(capitalize('ALL CAPS'));
