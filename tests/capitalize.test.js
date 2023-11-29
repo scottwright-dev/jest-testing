176e6889d39020abd4262d4119b17520e5c07335
Scott Wright <scott.wright.uk@gmail.com>
2023-11-29 09:22:02 +0000

    add: additional tests for capitalize function

--- tests/capitalize.test.js
+++ tests/capitalize.test.js
@@ -3,3 +3,19 @@ import capitalize from '../modules/capitalize';
 test('Expect capitalize function to be defined', () => {
     expect(capitalize).toBeDefined();
 });
+
+test('Expect capitalize function to return a string', () => {
+    const result = capitalize('hello');
+    expect(typeof result).toBe('string');
+});
+
+test('Expect first character to be capitalized', () => {
+    const result = capitalize('Hello');
+    expect(result.charAt(0)).toBe(result.charAt(0).toUpperCase());
+});
+
+test('Expect remaining letters of string to be returned as lowercase', () => {
+    const input = 'HELLO';
+    const result = capitalize(input);
+    expect(result.slice(1)).toBe(input.slice(1).toLowerCase());
+});
